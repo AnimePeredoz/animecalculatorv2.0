402fef4f7dbb142b457e25278cdccb814fe6f630
Max Yeager <79483961+AnimePeredoz@users.noreply.github.com>
2026-06-20 19:19:38 +0300

    script.js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ window.addEventListener('load', function () {
     const videoContainer = document.getElementById('loader-video-container');
     const video = document.getElementById('loader-video');
 
-    // Функция для скрытия видео
+  
     function hideLoader() {
         videoContainer.style.opacity = '0';
         setTimeout(() => {
             videoContainer.style.display = 'none';
-        }, 500); // Время должно совпадать с transition в CSS (0.5s)
+        }, 500); 
     }
 
-    // Если видео уже загрузилось быстрее, чем страница
+   
     if (video.ended) {
         hideLoader();
     } else {
-        // Ждем, пока видео доиграет до конца
+    
         video.addEventListener('ended', hideLoader);
     }
 });
